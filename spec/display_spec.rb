3f37ff80577e8983a41dcae7b3b18d70130d0721
Matthias Jenny <matthiascjenny@gmail.com>
2018-05-05 15:58:46 -0400

    :white_check_mark: Add display tests

---
+++ spec/display_spec.rb
@@ -0,0 +1,25 @@
+require 'display'
+require 'world'
+
+describe Display do
+
+  let(:initial_state) {
+    [
+      [:live, :dead, :dead],
+      [:dead, :dead, :dead],
+    ]
+  }
+  let(:world) { World.new(initial_state: initial_state) }
+  subject(:display) { Display.new(world: world) }
+
+
+  describe '#render' do
+    it 'renders two-dimensional grid' do
+      expect(display.render).to eq(
+        "\u{1f601} \u{1f480} \u{1f480} \n\u{1f480} \u{1f480} \u{1f480} "
+      )
+    end
+  end
+
+
+end
