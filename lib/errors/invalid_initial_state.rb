09fd0dd0f5b07a442f7f7f4555c6a913c5a24156
Matthias Jenny <matthiascjenny@gmail.com>
2018-05-05 16:47:19 -0400

    :recycle: Clean up inital state error

--- lib/errors/invalid_initial_state.rb
+++ lib/errors/invalid_initial_state.rb
@@ -1,8 +1,8 @@
 class InvalidInitialStateError < ArgumentError
   
   def msg
-    "Initial state must be a non-empty array all of whose members are non-empty 
-      arrays of equal length that only contain :live or :dead symbols"
+    "Initial state must be a non-empty array all of whose members are" +
+      "non-empty arrays of equal length that only contain :live or :dead symbols"
   end
 
 end
